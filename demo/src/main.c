9af29091621d89cb18060da422cb2d1890243fc1
Matthew Tole <mtole@pebble.com>
2016-05-10 15:58:13 -0700

    Update with latest changes to the Pebble library system

--- demo/src/main.c
+++ demo/src/main.c
@@ -1,41 +1,5 @@
-/*
-
-Scroll Text Layer 1.0.0
-A Pebble library for having a scrollable text layer in your Pebble apps.
-http://smallstoneapps.github.io/scroll-text-layer/
-
-----------------------
-
-The MIT License (MIT)
-
-Copyright © 2013-2014 Matthew Tole
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
---------------------
-
-sample/main.c
-
-*/
-
 #include <pebble.h>
-#include "../../src/scroll-text-layer.h"
+#include "../../include/scroll-text-layer.h"
 
 static void init(void);
 static void deinit(void);
@@ -106,7 +70,3 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   }
   scroll_text_layer_set_system_font(layer, fonts[font]);
 }
-
-
-
-
